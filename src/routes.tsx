aaa9fff90cd666a6fb118fe298208d3826df10c4
iamtheluiz <iamtheluiz.dev@gmail.com>
2021-12-18 15:26:35 -0300

    feat(frontend): add react-router-dom

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import { Home } from "./pages/Home";
+
+export const ApplicationRoutes = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+);
